5cf7d8f67567b93c7b81dc6c6701717e2bc3517e
Fairay <ffairay@gmail.com>
2021-05-25 20:41:00 +0300

    ADD: lab0

---
+++ lab9_1/md.c
@@ -0,0 +1,54 @@
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+
+#define		SHARED_IRQ	1
+
+static int irq = SHARED_IRQ, my_dev_id, irq_counter = 0;
+char my_tasklet_data[]="my_tasklet_function was called";
+
+// static
+// void my_tasklet_func(unsigned long);
+// DECLARE_TASKLET(my_tasklet, my_tasklet_func, (unsigned long) &my_tasklet_data);
+
+struct tasklet_struct my_tasklet; // dynamic
+
+void my_tasklet_func(unsigned long data)
+{
+	printk("tasklet data: %s\n", (char*)data);
+	printk(KERN_INFO "tasklet info: \tstate: %ld \tlink count: %d\n", my_tasklet.state, my_tasklet.count.counter);
+}
+
+static irqreturn_t my_interrupt (int irq, void *dev_id)
+{
+	irq_counter++;
+	printk(KERN_INFO "ISR: counter = %d\n", irq_counter);
+	tasklet_schedule(&my_tasklet);
+	return IRQ_HANDLED;
+}
+
+static int __init md_init(void) 
+{
+	if (request_irq(irq, my_interrupt, IRQF_SHARED, "my_interrupt", &my_dev_id))
+		return -1;
+
+	tasklet_init(&my_tasklet, my_tasklet_func, (unsigned long) &my_tasklet_data); // dynamic
+	printk(KERN_INFO "module is loaded\n");
+ 	return 0;
+}
+
+static void __exit md_exit(void) 
+{
+	tasklet_kill(&my_tasklet);
+	synchronize_irq(irq);
+	free_irq(irq, &my_dev_id);
+
+	printk(KERN_INFO "total amount of calls = %d\n", irq_counter);
+	printk(KERN_INFO "module is unloaded\n");
+}
+
+MODULE_LICENSE("GPL");
+module_init(md_init);
+module_exit(md_exit);
---
+++ lab9_2/md.c
@@ -0,0 +1,59 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/workqueue.h>
+#include <linux/interrupt.h>
+
+#define		SHARED_IRQ	1
+
+static int irq = SHARED_IRQ, my_dev_id, irq_counter = 0;
+static struct workqueue_struct *my_wq;
+
+void my_workqueue_func(struct work_struct *work)
+{
+	printk(KERN_INFO "work: data=%lld\n", work->data.counter);
+}
+DECLARE_WORK(my_workqueue, my_workqueue_func);
+
+static irqreturn_t my_interrupt (int irq, void *dev_id)
+{
+	irq_counter++;
+	printk(KERN_INFO "ISR: counter = %d\n", irq_counter);
+	queue_work(my_wq, &my_workqueue);
+	return IRQ_HANDLED;
+}
+
+static int __init md_init(void) 
+{
+	if (request_irq(irq, my_interrupt, IRQF_SHARED, "my_interrupt", &my_dev_id))
+	{
+		printk(KERN_ERR "request_irq failed\n");
+		return -1;
+	}
+
+	my_wq = create_workqueue("my_queue");
+	if (!my_wq)
+	{
+		free_irq(irq, &my_dev_id);
+		printk(KERN_ERR "create_workqueue failed\n");
+		return -1;
+	}
+
+	printk(KERN_INFO "module is loaded\n");
+ 	return 0;
+}
+
+static void __exit md_exit(void) 
+{
+	flush_workqueue(my_wq);
+	destroy_workqueue(my_wq);
+	synchronize_irq(irq);
+	free_irq(irq, &my_dev_id);
+
+	printk(KERN_INFO "total amount of calls = %d\n", irq_counter);
+	printk(KERN_INFO "module is unloaded\n");
+}
+
+MODULE_LICENSE("GPL");
+module_init(md_init);
+module_exit(md_exit);
